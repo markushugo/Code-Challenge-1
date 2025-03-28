075b06c4ec9e042a1f93a50d1d3004f2dab4cf9e
Markus Viktor Lynggaard <mal024@edu.zealand.dk>
2025-03-28 09:40:23 +0100

    Add project files.

---
+++ Code Challenge 1/MysteryBox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challenge_1
+{
+    class MysteryBox
+    {
+        private string _label;
+        private string _contains;
+        private bool _isExplosive;
+
+        public string Label
+        {
+            get { return _label; }
+            private set { _label = value; } 
+        }
+
+        public string Contains
+        {
+            get { return _contains; }
+            private set { _contains = value; }
+        }
+
+        public bool IsExplosive
+        {
+            get { return _isExplosive; }
+            private set { _isExplosive = value; }
+        }
+
+        public MysteryBox(string label, string contains, bool isExplosive)
+        {
+            Label = label;
+            Contains = contains;
+            IsExplosive = isExplosive;
+        }
+
+        public override string ToString()
+        {
+            return $"{Label}: Indeholder {Contains}, eksplosivitet: {IsExplosive}";
+        }
+    }
+}
---
+++ Code Challenge 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_Challenge_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MysteryBox mysteryBox1 = new MysteryBox("Mysterybox1", "Dynamit", true);
+            
+
+            Console.WriteLine(mysteryBox1);
+        }
+    }
+}
